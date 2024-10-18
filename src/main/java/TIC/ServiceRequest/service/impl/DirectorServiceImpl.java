48420c37a4da8fc03070220f1974b76f1cca9dce
KevinTurraca <kevin.itec1@gmail.com>
2024-10-18 09:10:32 -0300

    buscar por instituto añadido
    y muchos otros cambios menores

--- src/main/java/TIC/ServiceRequest/service/impl/DirectorServiceImpl.java
+++ src/main/java/TIC/ServiceRequest/service/impl/DirectorServiceImpl.java
@@ -1,6 +1,11 @@
 package TIC.ServiceRequest.service.impl;
 
+import java.util.List;
+
 import org.slf4j.Logger;
+
+import java.util.ArrayList;
+
 import org.slf4j.LoggerFactory;
 import lombok.extern.slf4j.Slf4j;
 import TIC.ServiceRequest.model.Director;
@@ -10,13 +15,10 @@ import TIC.ServiceRequest.service.DirectorService;
 import org.springframework.dao.DataAccessException;
 import TIC.ServiceRequest.repository.DirectorRepository;
 
-import static TIC.ServiceRequest.constant.Constant.*;
+import static TIC.ServiceRequest.constant.DirectorConstants.*;
 
-import java.util.List;
-import java.util.ArrayList;
-
-@Service
 @Slf4j
+@Service
 public class DirectorServiceImpl implements DirectorService {
 
     DirectorRepository repository;
@@ -26,7 +28,7 @@ public class DirectorServiceImpl implements DirectorService {
         repository = directorRepository;
     }
 
-
+    @Override
     public DirectorDTO save(DirectorDTO director) throws DataAccessException {
         try {
             Director newDirector = toEntity(director);
@@ -62,18 +64,33 @@ public class DirectorServiceImpl implements DirectorService {
         return null;
     }
 
+    public List<DirectorDTO> readByInstitute(String instituteCuise) throws DataAccessException {
+        try {
+            List<DirectorDTO> response = new ArrayList<>();
+            logger.info("Buscando todos los directivos pertenecientes a una Institución determinada");
+            for (Director director : repository.findAll()) {
+                if (director.getInstitute().getCuise().equals(instituteCuise)) {
+                    response.add(toDTO(director));
+                }
+            }
+            return response;
+        } catch (Exception e) {
+            searchError(e);
+        }
+        return new ArrayList<>();
+    }
+
     @Override
     public List<DirectorDTO> readAll() throws DataAccessException {
-
         try {
             List<DirectorDTO> response = new ArrayList<>();
             logger.info("Buscando todos los directivos");
-            for (Director director : repository.findAll()){
+            for (Director director : repository.findAll()) {
                 response.add(toDTO(director));
             }
             return response;
         } catch (Exception e) {
-            logger.error("Error: {}", e.getMessage());
+            searchError(e);
         }
         return new ArrayList<>();
     }
@@ -150,8 +167,8 @@ public class DirectorServiceImpl implements DirectorService {
     }
 
 
-    private void searchError(Exception e){
-        logger.error(ERROR_SEARCH+" {}", e.getMessage());
+    private void searchError(Exception e) {
+        logger.error(SERV_ERROR_SEARCH + " {}", e.getMessage());
     }
 
 }
