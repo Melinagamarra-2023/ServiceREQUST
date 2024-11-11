34778a90627dc51ac89f6477b46b1ace75803055
KevinTurraca <kevin.itec1@gmail.com>
2024-11-11 07:53:44 -0300

    actualización del wotkflow

--- src/test/java/TIC/ServiceRequest/service/impl/DirectorServiceImplTest.java
+++ src/test/java/TIC/ServiceRequest/service/impl/DirectorServiceImplTest.java
@@ -38,7 +38,7 @@ class DirectorServiceImplTest {
     }
 
     @Test
-    void correctCreate() {
+    void createSuccessful() {
         when(repository.save(any())).thenReturn(
                 new Director(1L, "123455", null, "", "", "", "", true));
         assertEquals("123455", service.create(directorsDTO.get(1)).getCuit());
@@ -46,14 +46,14 @@ class DirectorServiceImplTest {
     }
 
     @Test
-    void wrongCreate() {
+    void createWrong() {
         directorsDTO.get(0).setCuit("");
         assertNull(service.create(directorsDTO.get(0)));
         verify(repository, never()).save(any(Director.class));
     }
 
     @Test
-    void repeatedCreate() {
+    void createRepeated() {
         assertNull(service.create(directorsDTO.get(0)));
         verify(repository, never()).save(any(Director.class));
     }
@@ -67,7 +67,8 @@ class DirectorServiceImplTest {
 
     @Test
     void readOneNonExist() {
-
+        when(repository.findById(4L)).thenReturn(Optional.ofNullable(directors.get(3)));
+        assertNull(service.readOne(7L));
     }
 
     @Test
@@ -79,7 +80,8 @@ class DirectorServiceImplTest {
 
     @Test
     void readByCuitNonExist() {
-
+        assertNull(service.readByCuit("111"));
+        verify(repository, times(1)).findAll();
     }
 
     @Test
@@ -91,7 +93,9 @@ class DirectorServiceImplTest {
 
     @Test
     void readByInstituteNonExist() {
-
+        int expected = 0;
+        assertEquals(expected, service.readByInstitute("111").size());
+        verify(repository, times(1)).findAll();
     }
 
     @Test
@@ -101,16 +105,18 @@ class DirectorServiceImplTest {
         verify(repository, times(1)).findAll();
     }
 
-<<<<<<< Updated upstream
-/*
-   @Test
-=======
     @Test
->>>>>>> Stashed changes
-    void update() {
+    void updateSuccessful() {
         directorsDTO.get(0).setName("nuevo nombre");
         assertEquals("nuevo nombre", service.update("123452", directorsDTO.get(0)).getName());
         verify(repository, times(1)).save(any(Director.class));
+    }
+
+    @Test
+    void updateWrong() {
+        directorsDTO.get(0).setName("");
+        assertNull(service.update("123452", directorsDTO.get(0)));
+        verify(repository, never()).save(any(Director.class));
     }
 
     @Test
@@ -161,36 +167,43 @@ class DirectorServiceImplTest {
         Institute institute3 = new Institute();
         DirectorDTO directorDTO1 = new DirectorDTO();
         DirectorDTO directorDTO2 = new DirectorDTO();
+        //
         director1.setCuit("123451");
         director1.setName("1nombre");
         director1.setLastname("1apellido");
         director1.setMail("1@gmail.com");
         director1.setPhone("37641");
+        //
         director2.setCuit("123452");
         director2.setName("2nombre");
         director2.setLastname("2apellido");
         director2.setMail("2@gmail.com");
         director2.setPhone("37642");
+        //
         director3.setCuit("123453");
         director3.setName("3nombre");
         director3.setLastname("3apellido");
         director3.setMail("3@gmail.com");
         director3.setPhone("37643");
+        //
         director4.setCuit("123454");
         director4.setName("4nombre");
         director4.setLastname("4apellido");
         director4.setMail("4@gmail.com");
         director4.setPhone("37644");
+        //
         directorDTO1.setCuit("123452");
         directorDTO1.setName("2nombre");
         directorDTO1.setLastname("2apellido");
         directorDTO1.setMail("2@gmail.com");
         directorDTO1.setPhone("37642");
+        //
         directorDTO2.setCuit("123455");
         directorDTO2.setName("nombreNombre");
         directorDTO2.setLastname("apellidoApellido");
         directorDTO2.setMail("gmail@gmail.com");
         directorDTO2.setPhone("37643764");
+        //
         institute1.setId(1L);
         institute1.setCuise("112345");
         institute2.setId(2L);
@@ -203,6 +216,7 @@ class DirectorServiceImplTest {
         director4.setInstitute(institute2);
         directorDTO1.setInstitute(institute1);
         directorDTO2.setInstitute(institute3);
+        //
         directors.add(director1);
         directors.add(director2);
         directors.add(director3);
